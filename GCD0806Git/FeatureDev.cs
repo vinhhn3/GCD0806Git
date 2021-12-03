cc8db9c24292cc155cce4b2d631ad8efe7ef73a9
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-12-03 09:33:34 +0700

    Add feature FeatureDev

---
+++ GCD0806Git/FeatureDev.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GCD0806Git
+{
+  internal class FeatureDev
+  {
+    static void Print()
+    {
+      Console.WriteLine("This is a feature dev ...");
+    }
+  }
+}
